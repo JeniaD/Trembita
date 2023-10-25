ab8d9d5a292ed6b4de6f90f57d974967025d6d4c
JeniaD <jeniadeshchenko@gmail.com>
2023-10-25 18:15:55 +0200

    Fixed empty avatar glitch

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,11 +1,20 @@
-from flask import render_template, Blueprint, redirect, url_for, current_app, send_from_directory
+from flask import render_template, Blueprint, redirect, url_for, current_app, send_from_directory, send_file
 from flask_login import current_user
 from app.models import User, Post
+import os
 
 main = Blueprint("main", __name__)
 
 @main.route("/uploads/avatars/<filename>")
 def UploadedAvatar(filename):
+    filePath = os.path.join(current_app.config["UPLOAD_FOLDER"], f"avatars/{filename}.jpg")
+    
+    if os.path.exists(filePath):
+        send_file(filePath, mimetype="image/jpeg")
+    else:
+        filePath = os.path.join(current_app.config["UPLOAD_FOLDER"], f"avatars/default.png")
+        return send_file(filePath, mimetype="image/png")
+
     return send_from_directory(current_app.config["UPLOAD_FOLDER"], "avatars/" + filename + ".jpg")
 
 @main.route("/uploads/<filename>")
