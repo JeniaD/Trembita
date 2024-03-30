9c7edee247b46d269d8e7acd0eafa4e86ccd0173
JeniaD <jeniadeshchenko@gmail.com>
2024-03-30 17:58:18 +0100

    Cleaning

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
+# from flask_login import LoginManager
 from flask_babel import Babel
 from flask_jwt_extended import JWTManager
 
@@ -17,21 +17,13 @@ def create_app():
     db.init_app(app)
 
     # Register blueprints
-    from .routes.main import main as mainBlueprint
-    app.register_blueprint(mainBlueprint)
-    from .routes.auth import auth as authBlueprint
-    app.register_blueprint(authBlueprint)
-    from .routes.user import user as userBlueprint
-    app.register_blueprint(userBlueprint)
-    from .routes.errors import errors as errorsBlueprint
-    app.register_blueprint(errorsBlueprint)
     from .routes.api import api as apiBlueprint
     app.register_blueprint(apiBlueprint, url_prefix="/api")
 
     # Add login manager
-    loginManager = LoginManager()
-    loginManager.login_view = "auth.Login"
-    loginManager.init_app(app)
+    # loginManager = LoginManager()
+    # loginManager.login_view = "auth.Login"
+    # loginManager.init_app(app)
 
     # Initialize Babel
     babel = Babel(app)
@@ -40,9 +32,9 @@ def create_app():
     jwt = JWTManager(app)
 
     from .models import User
-    @loginManager.user_loader
-    def LoadUser(userID): return User.query.get(int(userID))
-    loginManager.login_message = "Будь ласка, увійдіть для перегляду сторінки"
+    # @loginManager.user_loader
+    # def LoadUser(userID): return User.query.get(int(userID))
+    # loginManager.login_message = "Будь ласка, увійдіть для перегляду сторінки"
 
     @jwt.user_identity_loader
     def UserIdentityLookup(user):
--- app/routes/errors.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-errors = Blueprint("errors", __name__)
-
-@errors.errorhandler(404)
-def PageNotFound(error):
-    return render_template("error.html", error=error), 404
